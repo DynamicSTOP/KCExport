6b4956caccb3e73c197498d7c3d9074195dfc5e7
Leonid <komsldt@gmail.com>
2018-06-02 19:47:25 +0300

    aws

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 // https://github.com/vuejs/vue-cli/blob/dev/docs/config.md
 module.exports = {
-    baseUrl: `/${process.env.APP_SUB_PATH}`,
+    baseUrl: `/${process.env.APP_SUB_PATH?process.env.APP_SUB_PATH:''}`,
 };
