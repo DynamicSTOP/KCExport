58318f24e772a4588e61fdf884dbf85fe55e9bf2
Leonid <komsldt@gmail.com>
2018-06-03 18:54:59 +0300

    ship sprites in blocks by 75

--- scripts/sprites.js
+++ scripts/sprites.js
@@ -37,7 +37,7 @@ const helpers = `
 }
 `;
 
-function _generatePartialSCSS(filename,params,image,filePath,prefixName) {
+function _generatePartialSCSS(filename, params, image, filePath, prefixName) {
     const name = prefixName + filename.split("\\").pop().split("/").pop().split(".").shift();
 
     return `
@@ -55,37 +55,87 @@ $${name}: (${params.x}px, ${params.y}px, ${-params.x}px, ${-params.y}px, ${param
 `;
 }
 
-function generateSCSS(imagesDirectory,spriteFilename,prefixName,padding=0){
-    Spritesmith.run({
-        src: fs.readdirSync(imagesDirectory).filter((s)=>s.indexOf("png")!==-1).map((s)=>imagesDirectory+'/'+s),
-        padding: padding
-    }, function handleResult(err, result) {
-        if(err) {
-            console.error(err);
-            process.exit(1);
-        }
-        let str=``, names=[];
-        fs.writeFileSync(__dirname + `/../src/sass/generated/${spriteFilename}`,result.image) ;
-        for(const i in result.coordinates){
-            if(result.coordinates.hasOwnProperty(i)){
-                str+=_generatePartialSCSS(i,result.coordinates[i],result.properties,`./generated/${spriteFilename}`,prefixName);
-                names.push(`$${prefixName}` + i.split("\\").pop().split("/").pop().split(".").shift());
+/**
+ *
+ * @param images
+ * @param spriteFilename
+ * @param prefixName
+ * @param num
+ * @param outputSCSS
+ * @param addScss
+ * @param padding
+ * @returns {Promise<any>}
+ */
+function generateSCSS(images, spriteFilename, prefixName, num=false, outputSCSS=true, addScss='', padding=0){
+    return new Promise((res) => {
+        Spritesmith.run({
+            src: images,
+            padding: padding
+        }, function handleResult(err, result) {
+            if(err) {
+                console.error(err);
+                process.exit(1);
             }
-        }
+            let str=``, names=[];
+            fs.writeFileSync(__dirname + `/../src/sass/generated/${spriteFilename}`,result.image) ;
+            for(const i in result.coordinates){
+                if(result.coordinates.hasOwnProperty(i)){
+                    str+=_generatePartialSCSS(i,result.coordinates[i],result.properties,`./generated/${spriteFilename}`,prefixName);
+                    names.push(`$${prefixName}` + i.split("\\").pop().split("/").pop().split(".").shift());
+                }
+            }
 
-        str+=`
-$spritesheet-width: ${result.properties.width}px;
-$spritesheet-height: ${result.properties.height}px;
-$spritesheet-image: './${spriteFilename}';
-$spritesheet-sprites: (${names.join(', ')}, );
-$spritesheet: (${result.properties.width}px, ${result.properties.height}px, './generated/${spriteFilename}', $spritesheet-sprites, );
+            const add = num === false ? '' : num;
+
+            str+=`
+$spritesheet${add}-width: ${result.properties.width}px;
+$spritesheet${add}-height: ${result.properties.height}px;
+$spritesheet${add}-image: './${spriteFilename}';
+$spritesheet${add}-sprites: (${names.join(', ')}, );
+$spritesheet${add}: (${result.properties.width}px, ${result.properties.height}px, './generated/${spriteFilename}', $spritesheet${add}-sprites, );
 
     `;
-        fs.writeFileSync(__dirname + `/../src/sass/generated/${spriteFilename.replace(/\.\w+$/,'.scss')}`,str+helpers) ;
-        console.log(`spriteFilename done.`);
+            if(outputSCSS){
+                console.log(spriteFilename);
+                return res(str);
+            } else {
+                fs.writeFileSync(__dirname + `/../src/sass/generated/${spriteFilename.replace(/\.\w+$/,'.scss')}`,str+addScss);
+                console.log(`sprite ${spriteFilename} done.`);
+                res();
+            }
+        });
     });
 }
 
-generateSCSS(__dirname + '/../src/images/ships','ship-sprite.png','ship');
+
+const blockSize = 75;
+const allShips = fs.readdirSync(__dirname + '/../src/images/ships').filter((s)=>s.indexOf("png")!==-1).map((s)=>Number(s.split(".").shift())).sort((a,b)=>a-b);
+console.log(`ships count ${allShips.length}`);
+const maxId = allShips[allShips.length-1];
+
+//i don't actually like js for stuff like this
+let shipBlocks = Array.from(Array(Math.ceil(maxId/blockSize)),()=>[]);
+
+allShips.map((shipId)=>{
+    const blockId = Math.floor(shipId/blockSize);
+    shipBlocks[blockId].push(__dirname + `/../src/images/ships/${shipId}.png`);
+});
+
+(async function(){
+    let shipsScss='';
+    let spriteSheets = `$spritesheets-sprites: (`;
+    for(let num=0;num<shipBlocks.length;num++){
+        if(shipBlocks[num].length===0)return;
+        shipsScss += await generateSCSS(shipBlocks[num], `ship-sprite.${num}.png`, 'ship', num);
+        spriteSheets += `$spritesheet${num}, `
+    }
+    spriteSheets+=");";
+
+    fs.writeFileSync(__dirname + `/../src/sass/generated/ship-sprite.scss`, shipsScss + helpers + spriteSheets);
+    console.log(`sprite ship-sprite.{x}.png done.`);
+
+})();
+
+const lockImages = fs.readdirSync(__dirname + '/../src/images/locks').filter((s)=>s.indexOf("png")!==-1).map((s)=>__dirname + '/../src/images/locks/'+s)
 //4px should prevent stupid overlapping in browser. haha chrome.
-generateSCSS(__dirname + '/../src/images/locks','ship-locks.png', 'lock', 4);
+generateSCSS(lockImages, 'ship-locks.png', 'lock', false, false, helpers, 4);
