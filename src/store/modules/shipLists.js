c6ae0615e8ea0e10e1a4275c2c0ced04be4f5bc8
Leonid <komsldt@gmail.com>
2018-10-26 22:04:41 +0300

    old updates

--- src/store/modules/shipLists.js
+++ src/store/modules/shipLists.js
@@ -2,6 +2,38 @@ import Vue from "vue";
 import ShipParser from '@/objects/ShipParser'
 import dataPacker from '@/datapacker/datapacker'
 
+class ShipList {
+    constructor(options) {
+        /**
+         * tiny hash from remote storage
+         * @type {String|null}
+         */
+        this.listId = options.listId || null;
+        /**
+         * encoded string for url exchange
+         * should contain all required info
+         * @type {string}
+         */
+        this.raw = options.raw || "";
+        /**
+         * custom user comment.
+         * current date by default
+         * @type {string}
+         */
+        this.comment = options.comment || `${(new Date()).toDateString()} ${(new Date()).toLocaleTimeString()}`;
+
+        /**
+         * Ships
+         * @type {Array}
+         */
+        this.ships = options.ships || [];
+    }
+
+    stringify() {
+        return JSON.stringify({list: this.listId, raw: this.raw, ships: this.ships, comment: this.comment});
+    }
+}
+
 const state = {
     /**
      * array of arrays. why ship as array? more easy to store and compare in local storage
@@ -9,57 +41,86 @@ const state = {
      */
     currentShipList: [],
     /**
-     * check local storage
-     * it should contain Array of Objects that have
-     * - Array .ships -> as currentShipList
-     * - String .comment -> by default date of creation
-     * - String .listId -> tiny hash from remote storage
+     * Array of ShipList objects
      */
     storedShipLists: []
 };
 
 const getters = {
-    shipList: state => ShipParser.buildShipObjects(state.currentShipList),
+    //shipList: state => ShipParser.buildShipObjects(state.currentShipList),
+    shipList: state => ShipParser.buildShipObjects(state.currentShipList.ships),
     storedShipLists: state => state.storedShipLists,
     isCurrentStored: function (state) {
-        const currentJSON = JSON.stringify(state.currentShipList);
+        const currentJSON = JSON.stringify(state.currentShipList.ships);
         return state.storedShipLists.filter((storedList) => JSON.stringify(storedList.ships) === currentJSON).length > 0;
     }
 };
 
+//async aren't allowed here
 const mutations = {
-    updateCurrentShipList(state, shipList) {
+    /**
+     * from anywhere inside app. ships are already in array form
+     * @param state
+     * @param ships
+     */
+    updateCurrentShipList(state, ships) {
         // in case it wasn't parsed
-        if (typeof shipList === "string") {
+        if (typeof ships === "string") {
             try {
-                const t = JSON.parse(shipList);
-                if (t instanceof Array) shipList = t;
+                const t = JSON.parse(ships);
+                if (t instanceof Array) ships = t;
                 else return;
             } catch (e) {
                 return;
             }
         }
-        state.currentShipList = shipList;
+        state.currentShipList = new ShipList({ships: ships});
         try {
-            localStorage.setItem('currentShipList', JSON.stringify(shipList));
+            localStorage.setItem('currentShipList', JSON.stringify(state.currentShipList));
         } catch (e) {
             console.error(e);
         }
     },
+
+    /**
+     * this might come through "NewTab & message channels"
+     * @param state
+     * @param ships
+     */
     updateCurrentShipListFromObject(state, ships) {
         if (typeof ships === "string") {
             ships = JSON.parse(ships);
         }
-        state.currentShipList = ShipParser.makeShipsArrays(ships);
+        state.currentShipList = new ShipList({ships: ShipParser.makeShipsArrays(ships)});
     },
 
+    /**
+     * On app startup it will try to load from local storage and update state
+     * @param state
+     * @param storedShipList
+     */
     updateStoredShipList(state, storedShipList) {
         state.storedShipLists = storedShipList;
     },
+
+    /**
+     * checks and adds new list to stored
+     * @param state
+     * @param newList
+     */
     saveCurrentShipList(state, newList) {
-        state.storedShipLists.push(newList);
-        this.commit('saveShipsToLocalStorage');
+        const shipsJSON = JSON.stringify(newList.ships);
+        if(state.storedShipLists.filter((storedList) => JSON.stringify(storedList.ships) === shipsJSON).length === 0){
+            state.storedShipLists.push(newList);
+            this.commit('saveShipsToLocalStorage');
+        }
     },
+
+    /**
+     * when got response from ajax ship shortify request
+     * @param state
+     * @param update
+     */
     updateShipsShortLink(state, update) {
         // object might be equal, but performing actions on it won't trigger observers
         // need to get real index
@@ -75,8 +136,14 @@ const mutations = {
         state.storedShipLists[newIndex].listId = update.listId;
         this.commit('saveShipsToLocalStorage');
     },
+
+    /**
+     *
+     * @param state
+     */
     saveShipsToLocalStorage(state) {
         try {
+            // we don't need raw here
             const listsToStore = state.storedShipLists.map((l) => {
                 return {
                     comment: l.comment,
@@ -89,14 +156,23 @@ const mutations = {
             console.error(e);
         }
     },
+
+    /**
+     * @param state
+     */
     clearCurrentShipList(state) {
-        state.currentShipList = [];
+        state.currentShipList = {};
         try {
             localStorage.removeItem('currentShipList');
         } catch (e) {
             console.error(e);
         }
     },
+
+    /**
+     * @param state
+     * @param list
+     */
     removeShipsList(state, list) {
         let newIndex = -1;
         for (let i = 0; i < state.storedShipLists.length; i++) {
@@ -212,7 +288,8 @@ const actions = {
         }
 
     },
-    async loadFromLocalStorage(context){
+
+    async loadFromLocalStorage(context) {
         if (localStorage.getItem('storedShipList')) {
             try {
                 let storedShipList = JSON.parse(localStorage.getItem('storedShipList')) || [];
@@ -227,7 +304,8 @@ const actions = {
             }
         }
     },
-    loadLastShips(context){
+
+    loadLastShips(context) {
         if (localStorage.getItem('currentShipList')) {
             try {
                 let currentShipList = JSON.parse(localStorage.getItem('currentShipList')) || [];
