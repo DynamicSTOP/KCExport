256a82b0da85a85a882423a405c1cf3e30b7abec
Leonid Babikov <komsldt@gmail.com>
2018-11-07 14:21:18 +0300

    removed message ports

--- src/components/NewTab.vue
+++ src/components/NewTab.vue
@@ -1,25 +1,24 @@
 <template>
     <div>
-        This page was opened to handle message Channels job. If you still reading this text then something probably went wrong.
+        This page was opened to handle message Channels job. If you still reading this text then something probably went
+        wrong.
     </div>
 </template>
 
 <script>
     export default {
         name: "NewTab",
-        data(){
-            return{
-                messageChannels: null
-            }
+        data() {
+            return {}
         },
         methods: {
             newMessage(message) {
                 try {
                     if (typeof(message.data.kc3assets) !== "undefined")
                         this.$store.dispatch('updateAssetsUrl', message.data.kc3assets);
-                    if (message.data.type === "KC3_SHIPS"){
+                    if (message.data.type === "KC3_SHIPS") {
                         this.$store.dispatch('updateCurrentShipListFromMessageChannel', message.data.ships);
-                        this.$router.push({name:"ShipList"});
+                        this.$router.push({name: "ShipList"});
                     }
                 } catch (e) {
                     return console.error(e);
@@ -27,15 +26,11 @@
             }
         },
         mounted() {
-            if (this.messageChannels === null) {
-                this.messageChannels = new MessageChannel();
-                this.messageChannels.port1.onmessage = this.newMessage;
-            }
-            window.opener.parent.postMessage("EXPORTER_STATE_READY", "*", [this.messageChannels.port2]);
+            window.addEventListener("message", this.newMessage);
+            window.opener.parent.postMessage("EXPORTER_STATE_READY", "*");
         },
         beforeDestroy() {
             window.removeEventListener("message", this.newMessage);
-            console.log(`listener removed`);
         }
     }
 </script>
