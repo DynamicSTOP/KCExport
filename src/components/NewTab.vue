8709e57dfad61fe2cf128875b60ff41220687a78
koms <komsldt@gmail.com>
2019-09-07 16:39:29 +0300

    additional ship value validation for "new tab"

--- src/components/NewTab.vue
+++ src/components/NewTab.vue
@@ -17,6 +17,26 @@
                     if (message.data.type === "KC3_DATA" && window.opener === message.source) {
                         if (typeof(message.data.kc3assets) !== "undefined")
                             this.$store.dispatch('updateAssetsUrl', message.data.kc3assets);
+                        // revalidate ship values
+                        message.data.ships = JSON.stringify(JSON.parse(message.data.ships).map((ship)=>{
+                            return {
+                                masterId: ship.masterId >= 0 ? ship.masterId : 0,
+                                id: ship.id >= 0 ? ship.id : 0,
+                                lvl: ship.lvl >= 0 ? ship.lvl : 0,
+                                sally: ship.sally >= 0 ? ship.sally : 0,
+                                
+                                aa: ship.aa >= 0 ? ship.aa : 0,
+                                ar: ship.ar >= 0 ? ship.ar : 0,
+                                tp: ship.tp >= 0 ? ship.tp : 0,
+                                fp: ship.fp >= 0 ? ship.fp : 0,
+                                
+                                extra_slot: ship.extra_slot >= 0 ? ship.extra_slot : 0,
+
+                                as: ship.as >= 0 ? ship.as : 0,
+                                hp: ship.hp >= 0 ? ship.hp : 0,
+                                lk: ship.lk >= 0 ? ship.lk : 0
+                            }
+                        }));
                         this.$store.dispatch('updateCurrentKCListFromMessageChannel', message.data);
                         this.$router.push({name: "ShipList"});
                     }
